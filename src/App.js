c39166fef4b4eff2cdc2d642ea1992e3557ba081
readwanmd <readwanmd@gmail.com>
2020-12-31 14:43:31 +0600

    fetch ui data done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './Components/Row/Row';
+import requests from './requests/requests';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<h1>Hay lets build netflix clone today!!!</h1>
+			<Row
+				title="NETFLIX ORGINALS"
+				fetchUrl={requests.fetchNetflixOrginals}
+				isLargeRow
+			/>
+			<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horor  Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Documentries" fetchUrl={requests.fetchDocumentaries} />
+		</div>
+	);
 }
 
 export default App;
